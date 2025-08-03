ea2c8ea413465f4294c35e847554871c75c11c4b
Marcio Hernani <marcio.hernani@gmail.com>
2025-08-03 18:26:28 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,11 @@
 async function fetchProfileData() {
   const url = 'https://githubusercontent.com/mhbsZSS/js-developer-portfolio/main/data/profile.json';
-  const fetching = await fetch(url)
-  return await fetching.json()
+  const response = await fetch(url)
+  const profileData = await response.json()
+  return profileData
 
 }
+
 
 
 
